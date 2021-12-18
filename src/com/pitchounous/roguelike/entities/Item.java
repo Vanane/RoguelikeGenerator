ff244a7ed4f3f9105e13a095a18b76f95c7c023f
Samuel Rigaud <rigaudsam@gmail.com>
2021-12-18 09:52:32 +0100

    First fully functionnal version with Ui implementation

--- src/com/pitchounous/roguelike/entities/Item.java
+++ src/com/pitchounous/roguelike/entities/Item.java
@@ -5,12 +5,12 @@ import java.util.Map;
 public class Item extends Entity {
 
 	private String effect;
-	
+
 	public Item(Map<String, String> itemData, int x, int y) {
 		super(itemData, x, y);
-        effect = itemData.get("effect");
+		effect = itemData.get("effect");
 	}
-	
+
 	public String getEffect() {
 		return effect;
 	}
--- src/com/pitchounous/roguelike/ui/Interface.java
+++ src/com/pitchounous/roguelike/ui/Interface.java
@@ -14,89 +14,91 @@ import java.util.Queue;
 public class Interface extends JFrame implements KeyListener, MouseListener {
 
 	private static final long serialVersionUID = 6408617006915516474L;
-	
+
 	private AsciiPanel terminal;
 	private AsciiCamera camera;
 	private Queue<InputEvent> inputQueue;
-	
+
 	private int screenWidth;
 	private int screenHeight;
 
-	
-    public Interface(int screenWidth, int screenHeight, Rectangle mapDimensions) {
-    	super("Roguelike");
-    	
-    	this.screenWidth = screenWidth;
-    	this.screenHeight = screenHeight;
-    	inputQueue = new LinkedList<>();
+	public Interface(int screenWidth, int screenHeight, Rectangle mapDimensions) {
+		super("Roguelike");
 
-    	Rectangle gameViewArea = new Rectangle(screenWidth, screenHeight-5);
-    	terminal = new AsciiPanel(screenWidth, screenHeight);
-    	camera = new AsciiCamera(mapDimensions, gameViewArea);
-    	
-        super.add(terminal);
-        super.addKeyListener(this);
-        super.addMouseListener(this);
-        super.setSize(screenWidth*9, screenHeight*16);
-        super.setVisible(true);
-        super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        super.repaint();
-    }
-    
-    public AsciiPanel getTerminal() {
-    	return terminal;
-    }
-    
-    public InputEvent getNextInput() {
-    	return inputQueue.poll();
-    }
-    
-    public void pointCameraAt(World world, int xfocus, int yfocus) {
-    	camera.lookAt(terminal, world, xfocus, yfocus);
-    }
-    
-    public void refresh() {
-    	terminal.repaint();
-    }
-    
+		this.screenWidth = screenWidth;
+		this.screenHeight = screenHeight;
+		inputQueue = new LinkedList<>();
+
+		Rectangle gameViewArea = new Rectangle(screenWidth, screenHeight - 5);
+		terminal = new AsciiPanel(screenWidth, screenHeight);
+		camera = new AsciiCamera(mapDimensions, gameViewArea);
+
+		super.add(terminal);
+		super.addKeyListener(this);
+		super.addMouseListener(this);
+		super.setSize(screenWidth * 9, screenHeight * 16);
+		super.setVisible(true);
+		super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		super.repaint();
+	}
+
+	public AsciiPanel getTerminal() {
+		return terminal;
+	}
+
+	public InputEvent getNextInput() {
+		return inputQueue.poll();
+	}
+
+	public void pointCameraAt(World world, int xfocus, int yfocus) {
+		camera.lookAt(terminal, world, xfocus, yfocus);
+	}
+
+	public void refresh() {
+		terminal.repaint();
+	}
+
 	@Override
 	public void keyPressed(KeyEvent e) {
 		inputQueue.add(e);
 	}
 
-	public void drawDynamicLegend(Rectangle gameViewArea, World world, Map<String, Map<String, String>> tileData, Map<String, Map<String, String>> creatureData) {
+	public void drawDynamicLegend(Rectangle gameViewArea, World world, Map<String, Map<String, String>> tileData,
+			Map<String, Map<String, String>> creatureData) {
 		int x = 5;
 		int y = gameViewArea.height;
 		char glyph;
-		
+
 		for (String tileType : world.getTileTypesInArea(gameViewArea)) {
 			glyph = tileData.get(tileType).get("glyph").charAt(0);
 			terminal.write(glyph + "   " + tileType, x, y);
 			y += 1;
-			
-			if (y == gameViewArea.height+2) {
+
+			if (y == gameViewArea.height + 2) {
 				x += 15;
 				y = gameViewArea.height;
 			}
 		}
-		
+
 		for (String creatureType : world.getCreatureTypesInArea(gameViewArea)) {
 			glyph = creatureData.get(creatureType).get("glyph").charAt(0);
 			terminal.write(glyph + "   " + creatureType, x, y);
 			y += 1;
-			
-			if (y == gameViewArea.height+5) {
+
+			if (y == gameViewArea.height + 5) {
 				x += 15;
 				y = gameViewArea.height;
 			}
 		}
 	}
-	
-	@Override
-	public void keyReleased(KeyEvent e) {}
 
 	@Override
-	public void keyTyped(KeyEvent e) {}
+	public void keyReleased(KeyEvent e) {
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
@@ -104,14 +106,18 @@ public class Interface extends JFrame implements KeyListener, MouseListener {
 	}
 
 	@Override
-	public void mouseEntered(MouseEvent e) {}
+	public void mouseEntered(MouseEvent e) {
+	}
 
 	@Override
-	public void mouseExited(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {
+	}
 
 	@Override
-	public void mousePressed(MouseEvent e) {}
+	public void mousePressed(MouseEvent e) {
+	}
 
 	@Override
-	public void mouseReleased(MouseEvent e) {}
+	public void mouseReleased(MouseEvent e) {
+	}
 }
